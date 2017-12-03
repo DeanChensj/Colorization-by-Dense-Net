0f28bb88e70934a26f47ccaed3ce510fdd198d9a
Dean Chen <463575680@qq.com>
2017-12-02 19:57:53 -0500

    Train skeleton

--- train.py
+++ train.py
@@ -1,34 +1,49 @@
 import argparse
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 from torch.autograd import Variable
 from model import Net
-from loss import MultinomialLoss
-
-
-def load_data():
-    return grey_img, color_img
+from skimage import color
+from util import load_data
+import time
 
 
 parser = argparse.ArgumentParser()
+# Optimization
 parser.add_argument(('-b', '--batch'), type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument(('-e', '--epoch'), type=int, default=200, metavar='N',
                     help='the number of training epoches')
 parser.add_argument(('-c', '--cuda'), type=bool, default=True, metavar='N',
                     help='whether to use cuda or not')
+parser.add_argument(('-lr', '--learning-rate'), type=float, default=0.01,
+                    help='learning rate (default: 0.01)')
+parser.add_argument(('-d', '--learning-rate-decay-factor'), type=float, default=0.5,
+                    help='learning rate decay factor (default: 0.5)')
+# Checkpoint
+parser.add_argument(('-cp', '--checkpoint-every'), type=int, default=200,
+                    help='checkpoint frequency')
+parser.add_argument(('-rc', '--resume-from-checkpoint'), type=str, default='',
+                    help='checkpoint file to resume from')
+
 args = parser.parse_args()
 
 batch_size = args.batch
 num_epoches = args.epoch
 use_cuda = args.cuda
+lr = args.learning_rate
+lr_decay = args.learning_rate_decay_factor
+checkpoint_every = args.checkpoint_every
+checkpoint_file = args.resume_from_checkpoint
 
-dense_net = DenseNet(use_cuda)
-mlp = Net(use_cuda)
-mtn_loss = MultinomialLoss(use_cuda)
+if checkpoint_file != '':
+    print('Loading checkpoint from %s' % checkpoint_file)
+    model = torch.load(checkpoint_file)
+else:
+    print('Initializing model from scratch')
+    model = Net(use_cuda)
+
+criterion = nn.MSELoss()
 optimizer = None
 
 if __name__ == "__main__":
@@ -36,25 +51,36 @@ if __name__ == "__main__":
     grey_img, color_img = load_data()
     num_imgs = grey_img.shape[0]
     num_batches = num_imgs // batch_size
+    loss_list = []
+
     # Convert to pytorch Variable while training
     for i in range(num_epoches):
         # Random shuffle data every epoch
         perm = torch.randperm(num_imgs)
         grey_img, color_img = grey_img[perm], color_img[perm]
+
+        loss_sum = 0.0
         for j in range(num_batches):
             grey_batch, color_batch = grey_img[j * batch_size:(
                 j + 1) * batch_size], color_img[j * batch_size:(j + 1) * batch_size]
             grey_batch, color_batch = Variable(
                 grey_batch), Variable(color_batch)
-            feature_list = dense_net(grey_batch)
-            # Reshape feature list if necessary
-            # Use FCL to do regression
+
+            output_batch = model(grey_batch)
             optimizer.zero_grad()
-            out_img = mlp(feature_list)
-            err = mtn_loss(out_img, color_batch)
+
+            err = criterion(output_batch, color_batch)
+            loss_sum += err.data[0]
             err.backward()
-            optimizer.step
-            # Print debug info
+            optimizer.step()
+
             t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
             print("[%s] Epoch %d, Batch %d, Loss = %lf" %
                   (t, i + 1, j + 1, err))
+
+        loss_sum /= num_batches
+        loss_list.append(loss_sum)
+        if i % checkpoint_every == 0:
+            torch.save(model, 'TrainedModel')
+    print(loss_list)
+
